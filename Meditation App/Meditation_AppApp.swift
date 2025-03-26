763e7154828230390f2a988d12833561718a7ce9
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-26 19:37:04 +0300

    Initial Commit

---
+++ Meditation App/Meditation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditation_AppApp.swift
+//  Meditation App
+//
+//  Created by mehmet Çelik on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Meditation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
